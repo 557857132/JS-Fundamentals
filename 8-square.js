ba54400fe318fd4c67c0026ccc16557bd47b7fb8
Emmanuel Fiifi Bentsi Essuman <deeptechhub5@gmail.com>
2025-09-14 03:58:04 +0000

    fix: Final attempt to fix 8-square checker

--- 8-square.js
+++ 8-square.js
@@ -3,11 +3,8 @@ const size = parseInt(process.argv[2]);
 if (isNaN(size) || size <= 0) {
   console.log("Missing size");
 } else {
+  const row = 'x'.repeat(size);
   for (let i = 0; i < size; i++) {
-    let row = '';
-    for (let j = 0; j < size; j++) {
-      row += 'x';
-    }
     console.log(row);
   }
 }
