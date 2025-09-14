4a8baf199249a6992eaf42a35abcb7eae6942161
Emmanuel Fiifi Bentsi Essuman <deeptechhub5@gmail.com>
2025-09-14 03:46:36 +0000

    fix: correct script to pass negative number check

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,10 +1,8 @@
-const arg = process.argv[2];
-const occurrences = parseInt(arg); // Attempt to convert the argument to an integer
+const occurrences = parseInt(process.argv[2]);
 
-if (isNaN(occurrences) || occurrences <= 0) {
+if (isNaN(occurrences)) {
   console.log("Missing number of occurrences");
-} else {
-  // Use a loop to print "C is fun" the specified number of times
+} else if (occurrences > 0) {
   for (let i = 0; i < occurrences; i++) {
     console.log("C is fun");
   }
